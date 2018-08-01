6f4308f2a49cddc24eacc29f70e0bd4068f0c70c
ONOUE Origami <onoueorigami@gmail.com>
2018-08-01 20:33:01 +0900

    storybook関連を大強化

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,6 @@
 import {configure} from '@storybook/react';
 
-// automatically import all files ending in *.stories.js
-const req = require.context('../src', true, /.stories.js$/);
+const req = require.context('../src', true, /.stories.tsx$/);
 
 function loadStories() {
 	req.keys().forEach(filename => req(filename));
---
+++ src/index.stories.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+
+import {storiesOf} from '@storybook/react';
+import {action} from '@storybook/addon-actions';
+import {linkTo} from '@storybook/addon-links';
+import {withInfo} from "@storybook/addon-info";
+
+const { Button, Welcome } = require('@storybook/react/demo');
+
+storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')}/>);
+
+storiesOf('Button', module)
+    .add('with text', withInfo('awesome text')(() => <Button onClick={action('clicked')}>Hello Button</Button>))
+    .add('with some emoji', () => (
+        <Button onClick={action('clicked')}>
+            <span role="img" aria-label="so cool">
+                😀 😎 👍 💯
+            </span>
+        </Button>
+    ));
