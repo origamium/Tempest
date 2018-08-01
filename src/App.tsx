781dfb0f658fd13f935e63301078bb069ab69468
ONOUE Origami <onoueorigami@gmail.com>
2018-08-01 19:59:30 +0900

    create react app with typescirptよ

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+class App extends React.Component {
+    public render() {
+        return (
+            <div className="App">
+                a
+            </div>
+        );
+    }
+}
+
+export default App;
