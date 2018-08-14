571c011d3be19b6229c5d7e941857aeb2bdc55de
ONOUE Origami <onoueorigami@gmail.com>
2018-08-14 17:21:20 +0900

    Markdownでstoryをあれするように

--- src/MainView/Molecule/ListItem/MiniContentCard/MiniContentCard.stories.tsx
+++ src/MainView/Molecule/ListItem/MiniContentCard/MiniContentCard.stories.tsx
@@ -1,5 +1,6 @@
 /*tslint:disable*/
 import * as React from 'react';
+import * as ReactMarkdown from 'react-markdown';
 import {storiesOf} from '@storybook/react';
 import {withKnobs, text} from '@storybook/addon-knobs';
 import {action} from '@storybook/addon-actions';
@@ -14,6 +15,18 @@ import Icons from '../../../../__testdata__/icon/icon';
 import Headers from '../../../../__testdata__/header/header';
 
 const sampleId = 'sample-id-0123456789';
+
+const description = `
+# Card Components
+Card コンポーネントは、引用されたステータス及びユーザー情報を表示するためのコンポーネントです。  
+
+## 制限
+横幅は100%です。Column Componentsの横幅によって大きさを変えるためです。
+`;
+
+storiesOf("Content/Card", module)
+    .add('description', () =>
+    <ReactMarkdown source={description} />);
 
 storiesOf("Content/Card/StatusCard", module)
     .addDecorator(centered)
