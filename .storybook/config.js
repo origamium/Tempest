1f1a60a57f51ba82563da7ba7e88a429a5e167b3
ONOUE Origami <onoueorigami@gmail.com>
2018-08-16 18:23:13 +0900

    addon-options導入忘れた

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,15 @@
 import {configure} from '@storybook/react';
+import {setOptions} from '@storybook/addon-options';
 
 const req = require.context('../src', true, /.stories.tsx$/);
 
 function loadStories() {
 	req.keys().forEach(filename => req(filename));
 }
+
+setOptions({
+    hierarchySeparator: /\/|\./,
+    hierarchyRootSeparator: /\|/,
+});
 
 configure(loadStories, module);
