2a01111eddf3e30a6123f0e7163ea98b2832bd9d
ONOUE Origami <onoueorigami@gmail.com>
2018-09-09 22:24:22 +0900

    やっています

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,13 @@
 import {configure} from '@storybook/react';
+import { setOptions } from '@storybook/addon-options';
 
 const req = require.context('../src', true, /.stories.tsx$/);
+
+setOptions({
+    name: 'tsuruclient/ui',
+    hierarchySeparator: /\/|\./,
+    hierarchyRootSeparator: /\|/,
+});
 
 function loadStories() {
 	req.keys().forEach(filename => req(filename));
