d9ffb574d8663b57ba3f3185ec83aea756b59861
Origami ONOUE <onoueorigami@gmail.com>
2018-09-23 03:09:23 +0900

    やっています

---
+++ src/Common/Molecule/Form/Form.tsx
@@ -0,0 +1,61 @@
+import * as React from 'react';
+import Dropzone from 'react-dropzone';
+import styled from 'styled-components';
+import Field from './Field';
+
+export type FormProps = {
+    /* unique key */
+    id: string,
+    /* accept file type.. */
+    accept: string,
+
+}
+
+type FormState = {
+    text: string,
+    file: File[],
+}
+
+const Styled = {
+    Root: styled(Dropzone)`
+        width: 100%;
+    `,
+};
+
+class Form extends React.Component<FormProps, FormState> {
+    constructor(props: FormProps) {
+        super(props);
+        this.state = {
+            text: "",
+            file: [],
+        };
+    }
+
+    render() {
+        const {id, accept} = this.props;
+        const {text} = this.state;
+        return (
+            <Styled.Root disableClick accept={accept}>
+                <Field id={id} value={text} handleChange={this.handleFieldChange}/>
+            </Styled.Root>
+        )
+    }
+
+    shouldComponentUpdate(nextProps: FormProps, nextState: FormState): boolean{
+        return true; // TODO
+    }
+
+    handleFieldChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+        this.setState({ text: event.target.value });
+    };
+
+    handleFileDrop = (): void => {
+
+    };
+
+    handleIsError = (): void => {
+
+    }
+}
+
+export default Form;
