6ed1432e3f8665aeb3d19a0716e0302d809ec82d
origamium <onoueorigami@gmail.com>
2019-02-17 21:25:29 +0900

    はい

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,22 +1,21 @@
 import { configure, addDecorator } from '@storybook/react';
-import { setOptions } from '@storybook/addon-options';
+import { withOptions } from '@storybook/addon-options';
 import { withInfo } from "@storybook/addon-info";
 import { withNotes } from "@storybook/addon-notes";
 import { withKnobs } from "@storybook/addon-knobs";
 import '../src/reset.css';
 const req = require.context('../src', true, /.stories.tsx$/);
 
-setOptions({
+addDecorator(withOptions({
     name: 'tsuruclient/ui',
     hierarchySeparator: /\/|\./,
     hierarchyRootSeparator: /\|/,
-});
+}));
+addDecorator(withNotes);
+addDecorator(withInfo);
+addDecorator(withKnobs);
 
 function loadStories() {
 	req.keys().forEach(filename => req(filename));
 }
-
-addDecorator(withNotes);
-addDecorator(withInfo);
-addDecorator(withKnobs);
 configure(loadStories, module);
