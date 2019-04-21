ec99c4a573240449a3f08f32c120607974f0a983
Origami ONOUE <onoueorigami@gmail.com>
2019-04-21 18:24:20 +0900

    はい

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -2,66 +2,57 @@ const path = require("path");
 const include = path.resolve(__dirname, '../');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
-module.exports = {
-	// Add '.ts' and '.tsx' as resolvable extensions.
-	resolve: {
-		extensions: [".css", ".ts", ".tsx", ".js"],
+module.exports = async ({config, mode}) => {
+    const newConfig = {...config}
+    newConfig.resolve = {
+        extensions: [".css", ".ts", ".tsx", ".js"],
         alias: {
             "@styled": path.resolve(__dirname, "../src/Theme")
         }
-	},
-	module: {
-		rules: [
-		    {
-                test: /\.css$/,
-                include,
-                use: [ {
-                    loader: 'style-loader'
-                }, {
-                    loader: 'css-loader'
-                } ]
-            },
-            {
-                test: /\.(jpe?g|png|gif|svg)$/,
-                include,
-                use: [
-                    {
-                        loader: 'url-loader'
+    }
+
+    newConfig.module.rules.unshift(...[
+        {
+            test: /\.css$/,
+            include,
+            use: [ {
+                loader: 'style-loader'
+            }, {
+                loader: 'css-loader'
+            } ]
+        },
+        {
+            test: /\.tsx?$/,
+            use: [
+                {
+                    loader: "ts-loader",
+                    options: {
+                        transpileOnly: true,
+                        configFile: __dirname + "/../tsconfig.json"
                     },
-                ]
-            },
-            {
-                test: /\.tsx?$/,
-                use: [
-                    {
-                        loader: "ts-loader",
-                        options: {
-                            transpileOnly: true,
-                            configFile: __dirname + "/../tsconfig.json"
-                        },
-                    },
-                    {
-                        loader: "react-docgen-typescript-loader"
-                    }
-                ],
-                exclude: /node_modules/,
-                include
-            },
-            {
-                test: /\.stories\.tsx?$/,
-                loaders: [
-                    {
-                        loader: require.resolve('@storybook/addon-storysource/loader'),
-                        options: {parser: 'typescript'}
-                    }
-                ],
-                enforce: 'pre',
-            }
-        ]
-	},
-    plugins: [
-        new ForkTsCheckerWebpackPlugin({
-            tsconfig: __dirname + "/../tsconfig.json"
-        })
-    ]
-};
+                },
+                {
+                    loader: "react-docgen-typescript-loader"
+                }
+            ],
+            exclude: /node_modules/,
+            include
+        },
+        {
+            test: /\.stories\.tsx?$/,
+            loaders: [
+                {
+                    loader: require.resolve('@storybook/addon-storysource/loader'),
+                    options: {parser: 'typescript'}
+                }
+            ],
+            enforce: 'pre',
+        }
+    ])
+
+    newConfig.plugins.push(new ForkTsCheckerWebpackPlugin({
+        tsconfig: __dirname + "/../tsconfig.json"
+    }))
+
+    return newConfig;
+}
