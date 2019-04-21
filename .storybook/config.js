8f98132281e15ac47f596dde7785f0b6c0e41c18
Origami ONOUE <onoueorigami@gmail.com>
2019-04-21 18:00:42 +0900

    はい

--- .storybook/config.js
+++ .storybook/config.js
@@ -4,6 +4,7 @@ import { withInfo } from "@storybook/addon-info";
 import { withNotes } from "@storybook/addon-notes";
 import { withKnobs } from "@storybook/addon-knobs";
 const req = require.context('../src', true, /.stories.tsx$/);
+import '!style-loader!css-loader!../src/reset.css';
 
 addDecorator(withOptions({
     name: 'tsuruclient/ui',
