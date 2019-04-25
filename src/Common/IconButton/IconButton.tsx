3c6a20a2f4330eafe8911f065014ff782f3ee57b
Origami ONOUE <onoueorigami@gmail.com>
2019-04-26 03:28:37 +0900

    はい

--- src/Common/IconButton/IconButton.tsx
+++ src/Common/IconButton/IconButton.tsx
@@ -1,40 +1,28 @@
-import * as React from 'react'
-import IconButtonHoC from './ReactionButtonHoCs/IconButtonHoC';
-import {Clear, Reply, Repeat, Favorite, Send, AttachFile} from "@material-ui/icons";
+import * as React from "react";
+import IconButtonHoC from "./ReactionButtonHoCs/IconButtonHoC";
+import { Clear, Reply, Repeat, Favorite, Send, AttachFile } from "@material-ui/icons";
 
-export type IconButtonStyle = {
-    activeColor?: string,
-    negativeColor: string,
-    size: string,
-};
+export interface IconButtonStyle {
+    activeColor?: string;
+    negativeColor: string;
+    size: string;
+}
 
-export type IconButtonProps = {
-    style: IconButtonStyle,
-    id: string,
-    active?: boolean,
-    handleClick: Function,
-};
+export interface IconButtonProps {
+    style: IconButtonStyle;
+    id: string;
+    active?: boolean;
+    handleClick: Function;
+}
 
-export const SendButton: React.SFC<IconButtonProps> = (props: IconButtonProps) => (
-    IconButtonHoC(Send)(props)
-);
+export const SendButton: React.FC<IconButtonProps> = (props: IconButtonProps) => IconButtonHoC(Send)(props);
 
-export const AttachFileButton: React.SFC<IconButtonProps> = (props: IconButtonProps) => (
-    IconButtonHoC(AttachFile)(props)
-);
+export const AttachFileButton: React.FC<IconButtonProps> = (props: IconButtonProps) => IconButtonHoC(AttachFile)(props);
 
-export const ClearButton: React.SFC<IconButtonProps> = (props: IconButtonProps) => (
-    IconButtonHoC(Clear)(props)
-);
+export const ClearButton: React.FC<IconButtonProps> = (props: IconButtonProps) => IconButtonHoC(Clear)(props);
 
-export const ReplyButton: React.SFC<IconButtonProps> = (props: IconButtonProps)=> (
-    IconButtonHoC(Reply)(props)
-);
+export const ReplyButton: React.FC<IconButtonProps> = (props: IconButtonProps) => IconButtonHoC(Reply)(props);
 
-export const RepeatButton: React.SFC<IconButtonProps> = (props: IconButtonProps)=> (
-    IconButtonHoC(Repeat)(props)
-);
+export const RepeatButton: React.FC<IconButtonProps> = (props: IconButtonProps) => IconButtonHoC(Repeat)(props);
 
-export const FavoriteButton: React.SFC<IconButtonProps> = (props: IconButtonProps)=> (
-    IconButtonHoC(Favorite)(props)
-);
+export const FavoriteButton: React.FC<IconButtonProps> = (props: IconButtonProps) => IconButtonHoC(Favorite)(props);
