bfab5578e9a4e7fa12c8a8fbb4689ad930ebab69
Origami ONOUE <onoueorigami@gmail.com>
2019-06-03 20:46:58 +0900

    WIP:はい

---
+++ src/MainView/ColumnList/ColumnList.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {AutoSizer, CellMeasurer, CellMeasurerCache, List} from "react-virtualized";
+import { IUICommonAttribuite } from "@tsuruclient/datatype";
+
+export interface ColumnList extends IUICommonAttribuite {
+
+}
+
+export const ColumnList: React.FC<ColumnList> {
+
+    return (<AutoSizer>
+
+    </AutoSizer>)
+}
