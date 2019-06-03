d1a2e8ad6c9afd2eb194277d57f3336a62219732
Origami ONOUE <onoueorigami@gmail.com>
2019-06-03 20:39:54 +0900

    はい

--- src/MainView/Column.tsx
+++ src/MainView/Column.tsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { styled } from "@styled";
 import { Paper } from "@material-ui/core";
 import { PaperProps } from "@material-ui/core/Paper";
+import { IUICommonAttribuite, IUser } from "@tsuruclient/datatype";
+import { Header } from "./ColumnHeader/Header";
+import { progressStatus } from "./ColumnHeader/StatusColorBar";
 
-export interface IColumnProps {
-    id: string;
+export interface IColumnProps extends IUICommonAttribuite {
+    name: string;
+    owner: IUser;
+    status: progressStatus;
     width: number;
 }
 
@@ -18,16 +23,41 @@ const Styled = {
         && {
             width: 100%;
             height: 100%;
+            display: flex;
+            flex-direction: column;
+            justify-content: flex-start;
+            & > header,
+            & > footer {
+                flex: 0 1 auto;
+            }
+
+            & > main {
+                height: 100%;
+                overflow: auto;
+                word-break: break-all;
+            }
         }
     `
 };
 
 const _Column: React.FC<IColumnProps> = (props: IColumnProps) => {
+    const [UIAttribute, setUIAttribute] = useState<IUICommonAttribuite>({
+        account: props.account,
+        column: props.column
+    });
+
+    useEffect(() => {
+        setUIAttribute({
+            account: props.account,
+            column: props.column
+        });
+    }, [props.account, props.column]);
+
     return (
         <Styled.Root width={props.width}>
             <Styled.Paper>
-                <header />
-                <main>はいじゃないが</main>
+                <Header {...UIAttribute} owner={props.owner} columnName={props.name} status={props.status} />
+                <main />
                 <footer />
             </Styled.Paper>
         </Styled.Root>
