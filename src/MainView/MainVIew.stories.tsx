fc60b98f5473fa11822a44ae1a0ee2a7c2387fdd
Origami ONOUE <onoueorigami@gmail.com>
2019-06-03 20:40:20 +0900

    はい

--- src/MainView/MainVIew.stories.tsx
+++ src/MainView/MainVIew.stories.tsx
@@ -1,16 +1,21 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
-import { select, boolean } from "@storybook/addon-knobs";
+import { boolean, select } from "@storybook/addon-knobs";
 import { Column } from "./Column";
-import { progressStatus, StatusColorBar } from "./ColumnParts/StatusColorBar";
+import { progressStatus, StatusColorBar } from "./ColumnHeader/StatusColorBar";
+import { IUICommonAttribuite } from "@tsuruclient/datatype";
+import { Header } from "./ColumnHeader/Header";
+import { User1 } from "../__testdata__/User";
+import centered from "@storybook/addon-centered";
 
 const StoryPrefix = "MainView|";
 
 const height100percent = (story) => <div style={{height: "100vh", display: "flex", alignItems: "center" }}>{story()}</div>
 
-storiesOf(StoryPrefix + "Column", module)
-    .addDecorator(height100percent)
-    .add("info", () => <Column id={"329005725180193470"} width={230}/>)
+const UIColumnAttribute: IUICommonAttribuite = {
+    account: "3902047509399",
+    column: "82477795091294"
+}
 
 const statusBarLabel = 'Status';
 const statusBarOptions = {
@@ -23,8 +28,25 @@ const statusBarDefaultValue = progressStatus.none;
 const raiseErrorLabel = "error";
 
 
+storiesOf(StoryPrefix + "Column", module)
+    .addDecorator(height100percent)
+    .add("info", () => <Column
+        {...UIColumnAttribute}
+        width={320}
+        owner={User1}
+        status={select(statusBarLabel, statusBarOptions, statusBarDefaultValue)}
+        name={"はいじゃない"} />)
+
 storiesOf(StoryPrefix + "ColumnHeader/StatusColorBar", module)
     .addDecorator(height100percent)
     .add("info", () => <StatusColorBar
         error={boolean(raiseErrorLabel, false)}
         status={select(statusBarLabel, statusBarOptions, statusBarDefaultValue)}/>)
+
+storiesOf(StoryPrefix + "ColumnHeader", module)
+.addDecorator(centered)
+.add("info", () => <Header
+    owner={User1}
+    columnName={"Home Timeline"}
+    status={progressStatus.inProgress}
+    {...UIColumnAttribute}/>)
