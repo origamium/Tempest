862175d2df3cf0495e3e33427e885acc7cc5300a
Origami ONOUE <onoueorigami@gmail.com>
2019-09-27 15:13:25 +0900

    はい

--- src/Common/Text/Text.stories.tsx
+++ src/Common/Text/Text.stories.tsx
@@ -5,7 +5,7 @@ import {text} from '@storybook/addon-knobs';
 import {action} from '@storybook/addon-actions';
 import centered from '@storybook/addon-centered';
 
-import Text from './Text';
+import {Text_ as Text} from './Text';
 
 const StoryPrefix = 'Common Components|Text';
 const initText = `はい #no https://google.com @arclisp`;
@@ -19,5 +19,5 @@ storiesOf(StoryPrefix, module)
     {}
     )
     .add('too long url', () => (
-        <Text text={'https://superloooooooooooooooooooooooooooooooooooooooooooongurl.origamium.com/'} handleLinkClick={action('clicked')}/>
+        <Text text={'https://superloooooooooooooooooooooooooooooooooooooooooooongurl.origamium.net/'} handleLinkClick={action('clicked')}/>
     ));
--- src/Common/Text/Text.tsx
+++ src/Common/Text/Text.tsx
@@ -1,22 +1,21 @@
-import * as React from 'react';
-import {onlyUpdateForKeys} from "recompose";
+import * as React from "react";
+import { onlyUpdateForKeys } from "recompose";
 import { Typography } from "@material-ui/core";
-import {ThemeStyle} from "@material-ui/core/styles/createTypography";
+import { ThemeStyle } from "@material-ui/core/styles/createTypography";
 
 export type TextProps = {
-    text: string,
-    variant?: ThemeStyle,
-    maxLinkLength?: number,
-    handleLinkClick: (href: string) => void,
-}
-
-export const Text = onlyUpdateForKeys(['text'])(
-    (props: TextProps) => (
-        <Typography variant={props.variant}>{props.text}</Typography>
-));
-
-Text.defaultProps = {
-    maxLinkLength: 64,
+    text: string;
+    variant?: ThemeStyle;
+    maxLinkLength?: number;
+    handleLinkClick: (href: string) => void;
 };
 
-export default Text;
+export const Text_: React.FC<TextProps> = props => {
+    return <Typography variant={props.variant}>{props.text}</Typography>;
+};
+
+Text_.defaultProps = {
+    maxLinkLength: 64
+};
+
+export const Text = onlyUpdateForKeys(["text"])(Text_);
