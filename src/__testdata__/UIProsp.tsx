cead1ec22f9aa002307af8785881e889c5c3dd10
Origami ONOUE <onoueorigami@gmail.com>
2019-09-28 15:50:49 +0900

    はい

---
+++ src/__testdata__/UIProsp.tsx
@@ -0,0 +1,12 @@
+import { IUICommonAttribuite, UIAction } from "@tsuruclient/datatype";
+
+const UICommonAttr: IUICommonAttribuite = {
+    account: "0",
+    column: "0"
+};
+
+const UIAction1: UIAction = {
+    id: "38e8e8898849898998288203902049275802",
+    name: "dymmmy",
+    action: () => console.log("yeah") // eslint-disable-line
+};
