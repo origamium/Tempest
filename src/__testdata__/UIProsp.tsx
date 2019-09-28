4bdac4ca8ca3081dfa4224f5bf71547b4355004d
Origami ONOUE <onoueorigami@gmail.com>
2019-09-28 15:51:52 +0900

    はい

--- src/__testdata__/UIProsp.tsx
+++ src/__testdata__/UIProsp.tsx
@@ -1,11 +1,11 @@
 import { IUICommonAttribuite, UIAction } from "@tsuruclient/datatype";
 
-const UICommonAttr: IUICommonAttribuite = {
+export const UICommonAttr: IUICommonAttribuite = {
     account: "0",
     column: "0"
 };
 
-const UIAction1: UIAction = {
+export const UIAction1: UIAction = {
     id: "38e8e8898849898998288203902049275802",
     name: "dymmmy",
     action: () => console.log("yeah") // eslint-disable-line
