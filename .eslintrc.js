9acaeba5022a4bdf57de4c3edbf3ec0e0f2a2fe8
ONOUE Origami <onoueorigami@gmail.com>
2020-03-06 23:44:39 +0900

    はい

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
     parser: "@typescript-eslint/parser",
     parserOptions: {
-        ecmaVersion: 6,
+        ecmaVersion: 2018,
         project: "./tsconfig.json",
         sourceType: "module",
         createDefaultProgram: true
@@ -44,20 +44,7 @@ module.exports = {
                 "react-hooks/exhaustive-deps": "warn",
                 "@typescript-eslint/explicit-function-return-type": "off",
                 "react/prop-types": "off"
-            }
-        },
-        ecmaVersion: 2018,
-        project: "./tsconfig.json",
-        sourceType: "module"
-    },
-    plugins: ["@typescript-eslint", "react-hooks"],
-    rules: {
-        "prettier/prettier": "error",
-        "ordered-imports": 0,
-        "object-literal-sort-keys": 0,
-        "no-console": ["warn", { allow: ["warn", "error"] }],
-        "react/prop-types": 0,
-        "@typescript-eslint/explicit-function-return-type": 0,
-        "@typescript-eslint/interface-name-prefix": 0
-    }
+            },
+        }
+    ]
 };
