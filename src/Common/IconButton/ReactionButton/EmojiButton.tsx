c8e42808dab321796cc5adf62f479cb31654ad28
origamium <onoueorigami@gmail.com>
2020-05-08 22:33:43 +0900

    feat: EmojiButtonHoCをEmojiButtonに変更

--- src/Common/IconButton/ReactionButton/EmojiButton.tsx
+++ src/Common/IconButton/ReactionButton/EmojiButton.tsx
@@ -1,15 +1,16 @@
 import * as React from "react";
 import { IconButton } from "@material-ui/core";
 
-type Props = {
+export type CharButtonProps = {
     handleClick: Function;
     id: string;
+    char: string; // TODO: URLにも対応させる
 };
 
-export const EmojiButtonHoC = (Emoji: String): Function => (props: Props) => {
+export const CharButton: React.FC<CharButtonProps> = (props) => {
     const handleClick = (e: React.MouseEvent<any>): void => {
         props.handleClick({ id: props.id });
     };
 
-    return <IconButton onClick={handleClick}>{Emoji}</IconButton>;
+    return <IconButton onClick={handleClick}>{props.char}</IconButton>;
 };
