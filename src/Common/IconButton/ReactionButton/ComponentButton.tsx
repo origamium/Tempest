01de36f528e88a4ceda320f3e656eebfe7a56dae
origamium <onoueorigami@gmail.com>
2020-05-08 22:30:19 +0900

    feat: IconButtonHocをComponentButtonに変更

--- src/Common/IconButton/ReactionButton/ComponentButton.tsx
+++ src/Common/IconButton/ReactionButton/ComponentButton.tsx
@@ -1,33 +1,31 @@
 import * as React from "react";
-import styled from "styled-components";
-import ButtonBase from "@material-ui/core/ButtonBase";
-import { SvgIconProps } from "@material-ui/core/SvgIcon/SvgIcon";
+import { styled, css } from "../../../Theme";
+import ButtonBase, { ButtonBaseProps } from "@material-ui/core/ButtonBase";
 import { IconButtonProps } from "../IconButton";
 
-const StyledButton = styled(ButtonBase)<any>`
+type StyledButtonAttr = { size: string | number; color: string };
+const StyledButton = styled(ButtonBase)<StyledButtonAttr>`
     && {
-        ${({ size, color }) => `
+        ${({ size, color }) => css`
             width: ${size};
             height: ${size};
             border-radius: 50%;
 
-            & > *{
+            & > * {
                 fill: ${color};
                 width: 100%;
                 height: 100%;
             }
         `}
     }
-`;
+` as React.FC<StyledButtonAttr & ButtonBaseProps & React.HTMLAttributes<HTMLButtonElement>>;
 
-const IconButtonHoC = (Icon: React.ComponentType<SvgIconProps>): Function => (props: IconButtonProps) => (
+export const ComponentButton: React.FC<IconButtonProps> = (props) => (
     <StyledButton
         color={props.active ? props.style.activeColor || props.style.negativeColor : props.style.negativeColor}
         size={props.style.size}
         onClick={props.handleClick}
     >
-        <Icon />
+        {props.children}
     </StyledButton>
 );
-
-export default IconButtonHoC;
