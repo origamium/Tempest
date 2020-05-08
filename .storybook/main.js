e0fcebb83abff79b6ad4331103f10213b090d034
origamium <onoueorigami@gmail.com>
2020-05-08 17:16:45 +0900

    feat: storybookの構成を変更

---
+++ .storybook/main.js
@@ -0,0 +1,20 @@
+module.exports = {
+    stories: ["../src/**/*.stories.tsx"],
+    addons: ["@storybook/addon-actions", "@storybook/addon-links"],
+    webpackFinal: async config => {
+        // do mutation to the config
+        config.module.rules.push({
+            test: /\.(ts|tsx)$/,
+            use: [
+                {
+                    loader: require.resolve("babel-loader")
+                },
+                {
+                    loader: require.resolve("react-docgen-typescript-loader")
+                }
+            ]
+        });
+        config.resolve.extensions.push(".ts", ".tsx");
+        return config;
+    },
+};
