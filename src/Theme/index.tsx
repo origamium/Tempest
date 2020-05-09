9a38b43860e1b976b1860f77ff6f84bd84ec3b09
origamium <onoueorigami@gmail.com>
2020-05-09 15:40:32 +0900

    feat: 独自のThemeProviderを定義

---
+++ src/Theme/index.tsx
@@ -0,0 +1,33 @@
+// from https://www.styled-components.com/docs/api#typescript
+import * as React from "react";
+import * as styledComponents from "styled-components";
+import { ITheme, IThemeSet } from "./ITheme";
+import { Theme } from "./Theme";
+import deepmerge from "deepmerge";
+
+const {
+    default: _styled,
+    css,
+    createGlobalStyle,
+    keyframes,
+    ThemeProvider: ThemeProvider_,
+} = styledComponents as styledComponents.ThemedStyledComponentsModule<ITheme>;
+
+export { css, createGlobalStyle, keyframes, Theme };
+export type { ITheme, IThemeSet };
+export const styled = _styled;
+
+export type ThemeProviderProps = {
+    theme: ITheme | keyof typeof Theme;
+};
+
+export const ThemeProvider: React.FC<ThemeProviderProps> = ({ theme: theme_, children }) => {
+    const theme = React.useMemo<ITheme>(
+        () => (typeof theme_ === "string" ? Theme[theme_] : (deepmerge(Theme.Light, theme_) as ITheme)),
+        []
+    );
+
+    return <ThemeProvider_ theme={theme}>{children}</ThemeProvider_>;
+};
+
+export const ThemeProviderDecorator = (storyFn: any) => <ThemeProvider theme={"Light"}>{storyFn()}</ThemeProvider>
