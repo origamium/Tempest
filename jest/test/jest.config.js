670b2493160fa767a361fd72cbf1e0dff6ec0d77
origamium <onoueorigami@gmail.com>
2020-05-29 20:27:30 +0900

    chore: jestの設定ファイルのインポート

---
+++ jest/test/jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    rootDir: "../../",
+    roots: ["<rootDir>/src"],
+    transform: {
+        "^.+\\.tsx?$": "ts-jest"
+    },
+    testEnvironment: "jsdom",
+    testRegex: "./*\\.(test|spec)\\.tsx?$",
+    moduleNameMapper: {
+        "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+            "<rootDir>/jest/fileMock.js",
+        "\\.(css|less)$": "<rootDir>/jest/styleMock.js"
+    },
+    setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
+    moduleFileExtensions: ["js", "ts", "tsx", "json"]
+};
