8e973d0c7f53acbebdbd41122f2427cfd1820d47
origamium <onoueorigami@gmail.com>
2020-06-12 19:20:08 +0900

    feat: 想定される例外を追加

---
+++ src/Redux/Exceptions.ts
@@ -0,0 +1,7 @@
+export const UnknownOAuthSignatureSpace = new Error('Unknown OAuth signature space');
+export const UnknownAuthorizationMethod = new Error('Unknown Authorization Method.');
+
+export const MultipleSandWitchParameterNotAllowed = new Error('Multiple SandWitched parameter is not allowed.');
+export const RequiredParameterNotFound = new Error('Required parameter not found.');
+export const NotDefinedParameterFound = new Error('Contains not defined parameters.');
+export const UnexpectedDataKey = new Error('Unexpected data key.');
