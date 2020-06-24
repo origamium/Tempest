21ba9fcf0c1ef79f93148009bc2c627c3b14dee2
origamium <onoueorigami@gmail.com>
2020-06-25 04:46:26 +0900

    fix: ApiSetObjectの設計見直し

--- src/Redux/Logics/Types/StoredObject/Service/ApiSet/ApiSetObject.ts
+++ src/Redux/Logics/Types/StoredObject/Service/ApiSet/ApiSetObject.ts
@@ -1,10 +1,7 @@
 import { AuthorizationUnitObject } from "./AuthorizationUnitObject";
-import { RestApiUnits } from "./RestApiUnitObject";
+import { APIDataType } from "../../../API/APIDataType";
 
 export type ApiSetObject = {
     authorization: AuthorizationUnitObject;
-    rest: RestApiUnits;
-    streaming?: object; // HTTP Streaming API. // TODO
-    rtc?: object; // real-time connection. // TODO
-    rcp?: object; // remote call procedure api. // TODO
+    apidata: APIDataType;
 };
