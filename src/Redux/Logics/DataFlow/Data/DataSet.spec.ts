dc59a830876437bf716eeb57a49715c1b3b3eb20
origamium <onoueorigami@gmail.com>
2020-07-03 19:39:37 +0900

    feat: ディレクトリ大粛清3

--- src/Redux/Logics/DataFlow/Data/DataSet.spec.ts
+++ src/Redux/Logics/DataFlow/Data/DataSet.spec.ts
@@ -1,6 +1,6 @@
 import DataSet from "./DataSet";
 import Twitter from "../DefaultSupport/Twitter";
-import * as data from "../../testdata/twitter/home_timeline.json";
+import * as data from "../testdata/twitter/home_timeline.json";
 const instance = new DataSet(Twitter.service.dataSet);
 
 describe("dynamizr working tests", () => {
