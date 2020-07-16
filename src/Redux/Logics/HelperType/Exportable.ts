4d7fa41b5a98629c9236c7328895f57cbf0f203c
origamium <onoueorigami@gmail.com>
2020-07-17 02:49:32 +0900

    feat: Exportableに変更を追加

--- src/Redux/Logics/HelperType/Exportable.ts
+++ src/Redux/Logics/HelperType/Exportable.ts
@@ -1,3 +1,7 @@
 export abstract class Exportable {
-    public abstract export(): string | object | never; // json.stringifyable
+    public abstract export(): object | never; // json.stringifyable
+}
+
+export abstract class ExportableRoot {
+    public abstract export(): string;
 }
