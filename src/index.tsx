35bc6113612445da7ae9aef94d6d453a7e5fce9f
origamium <onoueorigami@gmail.com>
2020-07-17 01:38:12 +0900

    feat: これも追加

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { App } from "./App";
 import { Provider } from "react-redux";
+import * as Sentry from "@sentry/react";
 import * as serviceWorker from "./serviceWorker";
 import { configureStore, sagaMiddleware } from "./Redux/Store/configureStore";
 import { rootSaga } from "./Redux/Sagas/rootSaga";
 import { Theme, ThemeProvider } from "./Theme";
 import "ress";
+
+Sentry.init({ dsn: "https://446c9a464ec844c2a5261438e9df8f52@o344655.ingest.sentry.io/5341100" });
 
 ReactDOM.render(
     <Provider store={configureStore()}>
