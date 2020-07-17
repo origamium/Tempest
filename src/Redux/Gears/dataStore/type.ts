ea780ea36581f9c696692683cac42d20da7f7475
origamium <onoueorigami@gmail.com>
2020-07-17 20:34:01 +0900

    feat: dataStoreにtypeを追加

---
+++ src/Redux/Gears/dataStore/type.ts
@@ -0,0 +1,14 @@
+import { Accounts } from "../../Logics/SavingData/StoredObject/Account/AccountObject";
+import { Services } from "../../Logics/SavingData/StoredObject/Service/ServiceObject";
+import { Providers } from "../../Logics/SavingData/StoredObject/Provider/ProviderObject";
+import { dbKeys } from "../../Store/indexedDB";
+import { UIObject } from "../../Logics/SavingData/StoredObject/UI/UIObject";
+
+export type RootObject = {
+    [dbKeys.account]: Accounts;
+    [dbKeys.service]: Services;
+    [dbKeys.provider]: Providers;
+    [dbKeys.ui]: UIObject;
+};
+
+export type PartialRootObject = Partial<RootObject>;
