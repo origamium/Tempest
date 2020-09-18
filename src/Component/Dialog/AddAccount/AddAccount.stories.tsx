7be7592f0c41e0fe8290a27056596feda55a7419
origamium <onoueorigami@gmail.com>
2020-09-18 23:22:05 +0900

    はい

--- src/Component/Dialog/AddAccount/AddAccount.stories.tsx
+++ src/Component/Dialog/AddAccount/AddAccount.stories.tsx
@@ -1,9 +1,11 @@
+import * as React from "react";
 import centered from "@storybook/addon-centered";
 import { ThemeProviderDecorator } from "../../../Theme";
+import { ServiceSelect } from "./ServiceSelect";
 
 export default {
     title: "Dialog/AddAccount",
     decorators: [centered, ThemeProviderDecorator],
 };
 
-
+export const ServiceSelectDialog = () => <ServiceSelect />;
--- src/Component/Dialog/AddAccount/ServiceSelect.tsx
+++ src/Component/Dialog/AddAccount/ServiceSelect.tsx
@@ -7,34 +7,104 @@ import {
     ListItem,
     ListItemText,
     ListItemAvatar,
+    ListItemIcon,
+    Collapse,
     Avatar,
-    Divider,
     Typography,
     Button,
+    Radio,
 } from "@material-ui/core";
+import { ExpandLess, ExpandMore } from "@material-ui/icons";
 import { useDialog } from "../../../hooks/useDialog";
+import { styled } from "../../../Theme";
 
-type ServiceView = {
+type ProviderListType = {
     key: string;
     name: string;
     icon?: string;
     description?: string;
 };
 
+type ServiceListType = {
+    service: string;
+    name: string;
+    providers: ProviderListType[];
+};
+
+type ProviderSelector = Array<ProviderListType | ServiceListType>;
+
 export type ServiceSelectProps = {};
+
+const Styled = {
+    Actions: styled.div`
+      width: 100%;
+      display: flex;
+      justify-content: space-between;
+`
+}
 
 export const ServiceSelect: React.FC<ServiceSelectProps> = () => {
     const [, , handleClose] = useDialog();
+
+    const [listOpen, setListOpen] = React.useState<string[]>([]);
+    const [selected, setSelect] = React.useState<string>();
+
+    const handleClickExpandableListItem = React.useCallback((e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+        const target = e.currentTarget.dataset.listname;
+        if (target) {
+            setListOpen((prev) => (prev.includes(target) ? prev.filter((v) => v !== target) : [...prev, target]));
+        }
+    }, []);
+
+    const handleSelectProvider = React.useCallback((e: React.SyntheticEvent<HTMLDivElement>) => {
+        const target = e.currentTarget.dataset.providerKey;
+        if (target) {
+            setSelect(target);
+        }
+    }, []);
+
+    const handleNext = React.useCallback(() => {
+        // todo
+    }, [])
+
     // todo
-    const services = React.useMemo<ServiceView[]>(() => {
+    const serviceAndProviders = React.useMemo<ProviderSelector>(() => {
         return [
             {
                 key: "twitter:twitter",
                 name: "Twitter",
             },
             {
-                key: "mastodon:pawoo",
-                name: "Pawoo",
+                service: "mastodon",
+                name: "Mastodon",
+                providers: [
+                    {
+                        key: "mastodon:pawoo",
+                        name: "Pawoo",
+                        icon: "https://image.internetcom.jp/upload/20170427/images/pawoo.jpg",
+                    },
+                    {
+                        key: "mastodon:poyo",
+                        name: "Poyo",
+                        description: "mastodon poyo server",
+                    },
+                ],
+            },
+            {
+                service: "gnu_social",
+                name: "GNU Social",
+                providers: [
+                    {
+                        key: "gnu_social:social",
+                        name: "Netflix",
+                        icon: "https://image.internetcom.jp/upload/20170427/images/pawoo.jpg",
+                    },
+                    {
+                        key: "gnu_social:wawawa",
+                        name: "Wawa",
+                        description: "mastodon poyo server",
+                    },
+                ],
             },
         ];
     }, []);
@@ -45,24 +115,64 @@ export const ServiceSelect: React.FC<ServiceSelectProps> = () => {
                 <Typography>{"Select Provider"}</Typography>
             </DialogTitle>
             <DialogContent>
-                <List>
-                    {services.map((item) => (
-                        <>
-                            <ListItem alignItems={"flex-start"}>
+                <List style={{ minWidth: "280px", maxWidth: "480px", width: "80%", overflow: "auto" }}>
+                    {serviceAndProviders.map((item, i) =>
+                        "service" in item ? (
+                            <>
+                                <ListItem
+                                    button
+                                    alignItems={"flex-start"}
+                                    data-listname={item.service}
+                                    onClick={handleClickExpandableListItem}
+                                >
+                                    <ListItemText primary={item.name} />
+                                    {listOpen.includes(item.service) ? <ExpandLess /> : <ExpandMore />}
+                                </ListItem>
+                                <Collapse in={listOpen.includes(item.service)} timeout="auto" unmountOnExit>
+                                    {item.providers.map((v, i) => (
+                                        <ListItem
+                                            key={i}
+                                            button
+                                            alignItems={"flex-start"}
+                                            data-provider-key={v.key}
+                                            onClick={handleSelectProvider}
+                                        >
+                                            <ListItemAvatar>
+                                                <Avatar src={v?.icon}>{v.name.slice(0, 2)}</Avatar>
+                                            </ListItemAvatar>
+                                            <ListItemText primary={v.name} secondary={v?.description} />
+                                            <Radio value={v.key} checked={v.key === selected} />
+                                        </ListItem>
+                                    ))}
+                                </Collapse>
+                            </>
+                        ) : (
+                            <ListItem
+                                key={i}
+                                button
+                                alignItems={"flex-start"}
+                                data-provider-key={item.key}
+                                onClick={handleSelectProvider}
+                            >
                                 <ListItemAvatar>
-                                    <Avatar src={item?.icon}>{item.name}</Avatar>
+                                    <Avatar src={item?.icon}>{item.name.slice(0, 2)}</Avatar>
                                 </ListItemAvatar>
                                 <ListItemText primary={item.name} secondary={item?.description} />
+                                <Radio value={item.key} checked={item.key === selected} />
                             </ListItem>
-                            <Divider />
-                        </>
-                    ))}
+                        )
+                    )}
                 </List>
             </DialogContent>
             <DialogActions>
-                <Button variant="contained" onClick={handleClose}>
-                    Cancel
-                </Button>
+                <Styled.Actions>
+                    <Button variant="contained" onClick={handleClose}>
+                        Cancel
+                    </Button>
+                    <Button variant="contained" color="primary" disabled={!selected} onClick={handleNext}>
+                        Next
+                    </Button>
+                </Styled.Actions>
             </DialogActions>
         </>
     );
