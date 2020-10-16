5683ab514e0cd1951d1e2f994ae7b69dd2cb4547
origamium <onoueorigami@gmail.com>
2020-10-16 18:59:27 +0900

    はい

--- src/Redux/rootSaga.ts
+++ src/Redux/rootSaga.ts
@@ -2,8 +2,11 @@ import { takeLatest } from "redux-saga/effects";
 import { dataStoreActionsIdentifier } from "./Slices/dataStore";
 import { requestRestoreSaga } from "./Slices/dataStore/requestRestore";
 import { requestInitializeSaga } from "./Slices/dataStore/requestInitialize";
+import { authorizationActionsIdentifier } from "./Slices/authorization";
+import { requestAuthorizationSequenceSaga } from "./Slices/authorization/requestAuthorizationSequence";
 
 export function* rootSaga() {
     yield takeLatest(dataStoreActionsIdentifier.REQUEST_RESTORE, requestRestoreSaga);
     yield takeLatest(dataStoreActionsIdentifier.REQUEST_INITIALIZE, requestInitializeSaga);
+    yield takeLatest(authorizationActionsIdentifier.REQUEST_AUTHORIZATION_SEQUENCE, requestAuthorizationSequenceSaga);
 }
