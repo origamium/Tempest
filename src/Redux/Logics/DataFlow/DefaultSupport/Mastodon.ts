c5b7e736e941aeb26a253e563a3bb480cbe32952
origamium <onoueorigami@gmail.com>
2020-11-04 03:04:19 +0900

    feat: Mastodonのservice, provider情報の追加

--- src/Redux/Logics/DataFlow/DefaultSupport/Mastodon.ts
+++ src/Redux/Logics/DataFlow/DefaultSupport/Mastodon.ts
@@ -2,10 +2,15 @@ import { APISetsObject } from "../Service/ApiSet/APISetObject";
 import { Protocol } from "../Types/Protocol";
 import { HttpMethods } from "../Types/HttpMethods";
 import { ProviderObject } from "../Provider/ProviderControl";
+import { OAuthVersion } from "../Types/Authorization/OAuthVersion";
+import { SignSpace } from "../Types/Authorization/SignSpace";
+import { SignMethod } from "../Types/Authorization/SignMethod";
+
+const serviceKey = "mastodon";
 
 export default {
     Service: {
-        serviceKey: "mastodon",
+        serviceKey,
         serviceName: "Mastodon",
         apiSet: {
             authorize: {
@@ -79,14 +84,42 @@ export default {
     },
     Provider: [
         {
+            serviceKey,
+            providerName: "mstdn.jp",
+            providerKey: "mstdnjp",
             baseUrl: "https://mstdn.jp/",
             domain: "mstdn.jp",
-            apiKey: undefined,
+            apiKey: {
+                ApiKey: "f24ec24cda87513ba5b963addc0b3a75d13c8fed83c17fc60ba2f7b59aeb7ffc",
+            },
+            authorization: {
+                apiUrl: "https://mstdn.net/",
+                oauthVersion: OAuthVersion.OAuth2,
+                signSpace: SignSpace.Header,
+                signMethod: SignMethod.plain,
+                callback: "urn:ietf:wg:oauth:2.0:oob",
+                requestAuthorizePagePath: "oauth/authorize",
+                requestAccessTokenPath: "oauth/token",
+            },
         },
         {
+            serviceKey,
+            providerName: "pawoo.net",
+            providerKey: "pawoonet",
             baseUrl: "https://pawoo.net/",
-            domain: "mstdn.jp",
-            apiKey: undefined,
+            domain: "pawoo.net",
+            apiKey: {
+                ApiKey: "5e0f667fb4da4b6e06cff473f8fdbce51f86c2af9c56ccd90b2fcad5160aeab8",
+            },
+            authorization: {
+                apiUrl: "https://pawoo.net/",
+                oauthVersion: OAuthVersion.OAuth2,
+                signSpace: SignSpace.Header,
+                signMethod: SignMethod.plain,
+                callback: "urn:ietf:wg:oauth:2.0:oob",
+                requestAuthorizePagePath: "oauth/authorize",
+                requestAccessTokenPath: "oauth/token",
+            },
         },
     ] as ProviderObject,
 };
