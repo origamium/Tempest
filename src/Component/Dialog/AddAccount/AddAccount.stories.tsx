1e90c437f59e48a18fd244daf61870f9eea9484f
origamium <onoueorigami@gmail.com>
2020-11-06 18:19:52 +0900

    feat: PINの入力ダイアログを実装

--- src/Component/Dialog/AddAccount/AddAccount.stories.tsx
+++ src/Component/Dialog/AddAccount/AddAccount.stories.tsx
@@ -2,10 +2,13 @@ import * as React from "react";
 import centered from "@storybook/addon-centered";
 import { ThemeProviderDecorator } from "../../../Theme";
 import { ServiceSelect } from "./ServiceSelect";
+import { action } from "@storybook/addon-actions";
+import { InputPIN } from "./InputPIN";
 
 export default {
     title: "Dialog/AddAccount",
     decorators: [centered, ThemeProviderDecorator],
 };
 
-export const ServiceSelectDialog = () => <ServiceSelect />;
+export const ServiceSelectDialog = () => <ServiceSelect list={[]} handleSubmitProvider={action("provider select!")} />;
+export const InputPINDialog = () => <InputPIN providerKey={"providerKey"} handleSubmitPIN={action("pin sended!")} />;
--- src/Component/Dialog/AddAccount/InputPIN.tsx
+++ src/Component/Dialog/AddAccount/InputPIN.tsx
@@ -1,5 +1,25 @@
 import * as React from "react";
+import { Button, DialogActions, DialogContent, DialogTitle, TextField, Typography } from "@material-ui/core";
 
-export const InputPIN: React.FC = () => {
-    return <div />;
+export type InputPINProps = {
+    providerKey: string;
+    handleSubmitPIN: (providerKey: string, code: string) => void;
+};
+
+export const InputPIN: React.FC<InputPINProps> = () => {
+    return (
+        <form>
+            <DialogTitle>
+                <Typography>{"Input PIN"}</Typography>
+            </DialogTitle>
+            <DialogContent>
+                <TextField label={"INSERT PIN HERE"} variant={"filled"} />
+                <DialogActions>
+                    <Button variant="contained" color="primary">
+                        {"Activate"}
+                    </Button>
+                </DialogActions>
+            </DialogContent>
+        </form>
+    );
 };
