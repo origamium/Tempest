c7ded74363c092a2922a1b0d74abebdbd890e744
origamium <onoueorigami@gmail.com>
2020-11-06 18:21:47 +0900

    feat: requestActivateCodeのactionなどを追加

---
+++ src/Redux/Slices/authorization/requestActivateCode.ts
@@ -0,0 +1,31 @@
+import { authorizationActionsIdentifier } from "./index";
+import { Action } from "redux";
+
+export interface RequestActivateCode extends Action {
+    type: authorizationActionsIdentifier.REQUEST_CODE_ACTIVATE;
+    payload: {
+        providerKey: string;
+        code: string;
+    };
+}
+
+export const requestActivateCode = (payload: { providerKey: string; code: string }): RequestActivateCode => ({
+    type: authorizationActionsIdentifier.REQUEST_CODE_ACTIVATE,
+    payload,
+});
+
+export interface SuccessActivateCode extends Action {
+    type: authorizationActionsIdentifier.SUCCESS_REQUEST_CODE_ACTIVATE;
+}
+
+export const successActivateCode = (): SuccessActivateCode => ({
+    type: authorizationActionsIdentifier.SUCCESS_REQUEST_CODE_ACTIVATE,
+});
+
+export interface FailedActivateCode extends Action {
+    type: authorizationActionsIdentifier.FAILED_REQUEST_CODE_ACTIVATE;
+}
+
+export const failedActivateCode = (): FailedActivateCode => ({
+    type: authorizationActionsIdentifier.FAILED_REQUEST_CODE_ACTIVATE
+});
