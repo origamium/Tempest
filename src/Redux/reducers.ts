73955f8bf988f1fef28e3d0ba55899273ff884e6
origamium <onoueorigami@gmail.com>
2020-11-12 00:38:30 +0900

    feat(redux): dialogreducer追加

--- src/Redux/reducers.ts
+++ src/Redux/reducers.ts
@@ -1,7 +1,9 @@
 import { Action, combineReducers } from "redux";
 import { dataStoreReducer } from "./Slices/dataStore/reducer";
 import { StoreType } from "./Store/StoreType";
+import { dialogReducer } from "./Slices/dialog/reducer";
 
 export const reducers = combineReducers<StoreType, Action>({
     dataStore: dataStoreReducer,
+    dialog: dialogReducer,
 });
