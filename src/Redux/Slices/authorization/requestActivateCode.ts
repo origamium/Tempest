50b488a08c59df88f17be298a160593adf5c3074
origamium <onoueorigami@gmail.com>
2020-11-13 00:36:39 +0900

    feat(saga): requestActivateCodeを追加

--- src/Redux/Slices/authorization/requestActivateCode.ts
+++ src/Redux/Slices/authorization/requestActivateCode.ts
@@ -1,15 +1,19 @@
 import { authorizationActionsIdentifier } from "./index";
 import { Action } from "redux";
+import { ServiceControl } from "../../Logics/DataFlow/Service/ServiceControl";
+import { ProviderControl } from "../../Logics/DataFlow/Provider/ProviderControl";
+import { select, call } from "redux-saga/effects";
+import { StoreType } from "../../Store/StoreType";
 
 export interface RequestActivateCode extends Action {
     type: authorizationActionsIdentifier.REQUEST_CODE_ACTIVATE;
     payload: {
-        providerKey: string;
+        key: string;
         code: string;
     };
 }
 
-export const requestActivateCode = (payload: { providerKey: string; code: string }): RequestActivateCode => ({
+export const requestActivateCode = (payload: { key: string; code: string }): RequestActivateCode => ({
     type: authorizationActionsIdentifier.REQUEST_CODE_ACTIVATE,
     payload,
 });
@@ -29,3 +33,37 @@ export interface FailedActivateCode extends Action {
 export const failedActivateCode = (): FailedActivateCode => ({
     type: authorizationActionsIdentifier.FAILED_REQUEST_CODE_ACTIVATE,
 });
+
+export function* requestActivateCodeSaga(action: RequestActivateCode) {
+    const { key, code } = action.payload;
+
+    const [serviceKey, providerKey] = key.split(",");
+
+    const [services, providers]: [
+        ServiceControl | undefined,
+        ProviderControl | undefined
+    ] = yield select((state: StoreType) =>
+        state.dataStore ? [state.dataStore.service, state.dataStore.provider] : [undefined, undefined]
+    );
+
+    const service = services?.getService(serviceKey);
+    const provider = providers?.getProvider(providerKey);
+
+    if (!provider || !service) {
+        // TODO: error
+        return;
+    }
+
+    const apiKey = "requestToken";
+    const getToken = service?.getApiSet(apiKey);
+
+    if (!getToken) {
+        throw new Error(`ApiSetControl.getApiSet(${apiKey}) is undefined`);
+    }
+
+    const [info, init] = provider.authorization.requestToken(getToken, code, providerKey);
+
+    const res = yield call(fetch, provider.authorization.authorizeLambda.requestAuthorizeTokenLambda ?? info, init);
+    const parsedResponse = yield call(service.parseResponse, getToken, res);
+    console.log(parsedResponse);
+}
--- src/Redux/rootSaga.ts
+++ src/Redux/rootSaga.ts
@@ -4,9 +4,11 @@ import { requestRestoreSaga } from "./Slices/dataStore/requestRestore";
 import { requestInitializeSaga } from "./Slices/dataStore/requestInitialize";
 import { authorizationActionsIdentifier } from "./Slices/authorization";
 import { requestAuthorizationSequenceSaga } from "./Slices/authorization/requestAuthorizationSequence";
+import { requestActivateCodeSaga } from "./Slices/authorization/requestActivateCode";
 
 export function* rootSaga() {
     yield takeLatest(dataStoreActionsIdentifier.REQUEST_RESTORE, requestRestoreSaga);
     yield takeLatest(dataStoreActionsIdentifier.REQUEST_INITIALIZE, requestInitializeSaga);
     yield takeLatest(authorizationActionsIdentifier.REQUEST_AUTHORIZATION_SEQUENCE, requestAuthorizationSequenceSaga);
+    yield takeLatest(authorizationActionsIdentifier.REQUEST_CODE_ACTIVATE, requestActivateCodeSaga);
 }
