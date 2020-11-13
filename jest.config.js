d091c1929849f5212dbb2dfcc546f9afe8b3ee99
origamium <onoueorigami@gmail.com>
2020-11-13 14:52:53 +0900

    fix: jest.config.jsの修正

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,15 @@
 module.exports = {
-    rootDir: "../../",
     roots: ["<rootDir>/src"],
     transform: {
-        "^.+\\.tsx?$": "ts-jest"
+        "^.+\\.tsx?$": "babel-jest",
     },
     testEnvironment: "jsdom",
     testRegex: "./*\\.(test|spec)\\.tsx?$",
     moduleNameMapper: {
         "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
             "<rootDir>/jest/fileMock.js",
-        "\\.(css|less)$": "<rootDir>/jest/styleMock.js"
+        "\\.(css|less)$": "<rootDir>/jest/styleMock.js",
     },
     setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
-    moduleFileExtensions: ["js", "ts", "tsx", "json"]
+    moduleFileExtensions: ["js", "ts", "tsx", "json"],
 };
