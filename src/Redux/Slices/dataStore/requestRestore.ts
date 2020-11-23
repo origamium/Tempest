22c0249dc3078c0b8eee0a919fd78681c0f2e5eb
origamium <onoueorigami@gmail.com>
2020-11-23 14:58:22 +0900

    chore: requestRestoreでserviceとproviderは常に最新を保つように

--- src/Redux/Slices/dataStore/requestRestore.ts
+++ src/Redux/Slices/dataStore/requestRestore.ts
@@ -13,6 +13,8 @@ import { AccountControl } from "../../Logics/DataFlow/Account/AccountControl";
 import { ServiceControl } from "../../Logics/DataFlow/Service/ServiceControl";
 import { ProviderControl } from "../../Logics/DataFlow/Provider/ProviderControl";
 import { ContentsControl } from "../../Logics/DataFlow/Contents/ContentsControl";
+import twitter from "../../Logics/DataFlow/DefaultSupport/Twitter";
+import mastodon from "../../Logics/DataFlow/DefaultSupport/Mastodon";
 
 export interface RequestRestoreActionType extends Action {
     type: dataStoreActionsIdentifier.REQUEST_RESTORE;
@@ -29,8 +31,14 @@ export function* requestRestoreSaga() {
         if (keys.length > 0) {
             const uiObj: UIObject = yield call([SettingStore, SettingStore.getItem], dbKeys.ui);
             const accountObj = yield call([SettingStore, SettingStore.getItem], dbKeys.account);
-            const serviceObj = yield call([SettingStore, SettingStore.getItem], dbKeys.service);
-            const providerObj = yield call([SettingStore, SettingStore.getItem], dbKeys.provider);
+
+            // in development mode
+            // const serviceObj = yield call([SettingStore, SettingStore.getItem], dbKeys.service);
+            // const providerObj = yield call([SettingStore, SettingStore.getItem], dbKeys.provider);
+
+            const service = new ServiceControl({ twitter: twitter.service, mastodon: mastodon.Service });
+            const provider = new ProviderControl({ twitter: twitter.provider, mastodon: mastodon.Provider });
+
             const contentObj = yield call([SettingStore, SettingStore.getItem], dbKeys.content);
 
             const page = new PageControl(uiObj.page, uiObj.columns, uiObj.mutes);
@@ -40,8 +48,6 @@ export function* requestRestoreSaga() {
 
             const account = new AccountControl(accountObj);
             const content = new ContentsControl(contentObj);
-            const service = new ServiceControl(serviceObj);
-            const provider = new ProviderControl(providerObj);
 
             yield put(finishRestoreAction({ page, tabs, columns, mutes, account, content, service, provider }));
         } else {
