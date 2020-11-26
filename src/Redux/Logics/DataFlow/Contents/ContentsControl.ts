0f10f8dd68381afebd4ca475ffa7b2a283e3b80a
ONOUE Origami <onoueorigami@gmail.com>
2020-11-26 23:25:30 +0900

    はい

--- src/Redux/Logics/DataFlow/Contents/ContentsControl.ts
+++ src/Redux/Logics/DataFlow/Contents/ContentsControl.ts
@@ -5,6 +5,7 @@ export type ContentsStruct = {
     account: string;
     maxListLength: number; //default: 1000
     contentsKey: string;
+    isList: boolean;
     content?: any;
 };
 
@@ -14,12 +15,14 @@ export class Contents implements Exportable<ContentsStruct> {
     private _account: string;
     private _contentsKey: string;
     private _contents: any;
+    private _isList: boolean;
     private _maxListLength: number;
 
-    constructor(source: ContentsStruct, update?: { content: any }) {
+    constructor(source: ContentsStruct, renew?: { content: any }) {
         this._account = source.account;
         this._maxListLength = source.maxListLength;
         this._contentsKey = source.contentsKey;
+        this._isList = source.isList;
         if (source.content) {
             this._contents = source.content;
         }
@@ -34,11 +37,15 @@ export class Contents implements Exportable<ContentsStruct> {
     }
 
     public updateContent(update: { content: any | any[] }): Contents {
-        if (Array.isArray(update.content)) {
+        if (this._isList) {
             return new Contents(this.export(), { content: [...this._contents, ...update.content] });
         } else {
             return new Contents(this.export(), { content: update.content });
         }
+    }
+
+    public renew(): Contents {
+        return new Contents(this.export(), { content: this.contents });
     }
 
     export(): ContentsStruct {
@@ -46,6 +53,7 @@ export class Contents implements Exportable<ContentsStruct> {
             account: this._account,
             maxListLength: this._maxListLength,
             contentsKey: this._contentsKey,
+            isList: this._isList,
         };
     }
 }
@@ -62,11 +70,24 @@ export class ContentsControl implements Exportable<ContentsControlObject> {
                 .reduce(
                     (accm, [, curr]) => ({
                         ...accm,
-                        [curr!.contentsKey]: new Contents(curr!),
+                        [curr!.contentsKey]: curr!,
                     }),
                     {}
                 );
         }
+    }
+
+    public updateContent(key: string, content: any): ContentsControl {
+        return new ContentsControl(
+            {},
+            Object.entries(this.contents).reduce(
+                (accm, [currKey, currValue]) => ({
+                    ...accm,
+                    [currKey]: currKey === key ? currValue.updateContent({ content }) : currValue.renew(),
+                }),
+                {}
+            )
+        );
     }
 
     export(): ContentsControlObject {
