da9cf00ecf9ad41ac26f596bf233d067df235075
origami onoue <arclisp@plotdot.net>
2023-07-14 13:01:21 +0900

    feat: add react-query

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,15 @@ import ReactDOM from "react-dom";
 import { App } from "./App";
 import { Theme, ThemeProvider } from "./Theme";
 import "ress";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.render(
-    <ThemeProvider theme={Theme.Light}>
-        <App />
-    </ThemeProvider>,
-    document.getElementById("root")
+    <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={Theme.Light}>
+            <App />
+        </ThemeProvider>
+    </QueryClientProvider>,
+    document.getElementById("root"),
 );
